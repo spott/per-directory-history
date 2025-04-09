4117b0683bfd72e32dbbf2e73f477535e966c841
Andrew Spott <andrew.spott@gmail.com>
2025-04-09 14:48:13 -0600

    added support for git repos.  if you are in a git repo, then there is a local git history, otherwise it uses a directory history, or a global history

--- per-directory-history.zsh
+++ per-directory-history.zsh
@@ -1,54 +1,23 @@
 #!/usr/bin/env zsh
 #
-# This is a implementation of per directory history for zsh, some
-# implementations of which exist in bash[1,2].  It also implements
-# a per-directory-history-toggle-history function to change from using the
-# directory history to using the global history.  In both cases the history is
-# always saved to both the global history and the directory history, so the
-# toggle state will not effect the saved histories.  Being able to switch
-# between global and directory histories on the fly is a novel feature as far
-# as I am aware.
+# This is a implementation of per directory history for zsh, with support for per-git-repo history.
+# When in a git repo, history is stored locally in the git repo instead of in the default location.
+# Original implementation by Jim Hester, modified to support git repos.
+#
+# It also implements a per-directory-history-toggle-history function to change from using the
+# directory/repo history to using the global history. In both cases the history is
+# always saved to both the global history and the local history, so the
+# toggle state will not effect the saved histories.
 #
 #-------------------------------------------------------------------------------
 # Configuration
 #-------------------------------------------------------------------------------
 #
 # HISTORY_BASE a global variable that defines the base directory in which the
-# directory histories are stored
+# directory histories are stored (for non-git directories)
 #
-#-------------------------------------------------------------------------------
-# History
-#-------------------------------------------------------------------------------
-#
-# The idea/inspiration for a per directory history is from Stewart MacArthur[1]
-# and Dieter[2], the implementation idea is from Bart Schaefer on the the zsh
-# mailing list[3].  The implementation is by Jim Hester in September 2012.
-#
-# [1]: http://www.compbiome.com/2010/07/bash-per-directory-bash-history.html
-# [2]: http://dieter.plaetinck.be/per_directory_bash
-# [3]: http://www.zsh.org/mla/users/1997/msg00226.html
-#
-################################################################################
-#
-# Copyright (c) 2014 Jim Hester
-#
-# This software is provided 'as-is', without any express or implied warranty.
-# In no event will the authors be held liable for any damages arising from the
-# use of this software.
-#
-# Permission is granted to anyone to use this software for any purpose,
-# including commercial applications, and to alter it and redistribute it
-# freely, subject to the following restrictions:
-#
-# 1. The origin of this software must not be misrepresented; you must not claim
-# that you wrote the original software. If you use this software in a product,
-# an acknowledgment in the product documentation would be appreciated but is
-# not required.
-#
-# 2. Altered source versions must be plainly marked as such, and must not be
-# misrepresented as being the original software.
-#
-# 3. This notice may not be removed or altered from any source distribution..
+# GIT_HISTORY_NAME the name of the history file to create in git repositories
+# (defaults to .git_zsh_history)
 #
 ################################################################################
 
@@ -57,8 +26,71 @@
 #-------------------------------------------------------------------------------
 
 [[ -z $HISTORY_BASE ]] && HISTORY_BASE="$HOME/.directory_history"
+[[ -z $GIT_HISTORY_NAME ]] && GIT_HISTORY_NAME=".zsh_history"
 [[ -z $HISTORY_START_WITH_GLOBAL ]] && HISTORY_START_WITH_GLOBAL=false
 [[ -z $PER_DIRECTORY_HISTORY_TOGGLE ]] && PER_DIRECTORY_HISTORY_TOGGLE='^G'
+
+#-------------------------------------------------------------------------------
+# Function to determine if current directory is in a git repo
+# and get the repository root if so
+#-------------------------------------------------------------------------------
+
+function _is_in_git_repo() {
+  git rev-parse --is-inside-work-tree &>/dev/null
+  return $?
+}
+
+function _is_in_git_repo_at_path() {
+  local path="$1"
+  git -C "$path" rev-parse --is-inside-work-tree &>/dev/null
+  return $?
+}
+
+function _get_git_repo_root() {
+  git rev-parse --show-toplevel
+}
+
+function _get_git_repo_root_at_path() {
+  local path="$1"
+  git -C "$path" rev-parse --show-toplevel 2>/dev/null
+}
+
+#-------------------------------------------------------------------------------
+# Function to set the appropriate history file path
+#-------------------------------------------------------------------------------
+
+function _set_per_directory_history_path() {
+  if _is_in_git_repo; then
+    # Get git repo root
+    local repo_root=$(_get_git_repo_root)
+    _per_directory_history_directory="${repo_root}/${GIT_HISTORY_NAME}"
+  else
+    # Use traditional per-directory history
+    _per_directory_history_directory="$HISTORY_BASE${PWD:A}/history"
+  fi
+  
+  # Ensure parent directory exists if needed
+  if [[ $_per_directory_history_directory == */* ]]; then
+    mkdir -p ${_per_directory_history_directory:h}
+  fi
+}
+
+#-------------------------------------------------------------------------------
+# Function to get history path for a specific directory
+#-------------------------------------------------------------------------------
+function _get_history_file_for_directory() {
+  local directory="$1"
+  local history_path
+  
+  if _is_in_git_repo_at_path "$directory"; then
+    local gitroot=$(_get_git_repo_root_at_path "$directory")
+    history_path="${gitroot}/${GIT_HISTORY_NAME}"
+  else
+    history_path="$HISTORY_BASE${directory:A}/history"
+  fi
+  
+  echo "$history_path"
+}
 
 #-------------------------------------------------------------------------------
 # toggle global/directory history used for searching - ctrl-G by default
@@ -69,7 +101,11 @@ function per-directory-history-toggle-history() {
     _per-directory-history-set-directory-history
     _per_directory_history_is_global=false
     zle -I
-    echo "using local history"
+    if _is_in_git_repo; then
+      echo "using git repo history"
+    else
+      echo "using local directory history"
+    fi
   else
     _per-directory-history-set-global-history
     _per_directory_history_is_global=true
@@ -87,38 +123,47 @@ bindkey -M vicmd $PER_DIRECTORY_HISTORY_TOGGLE per-directory-history-toggle-hist
 # implementation details
 #-------------------------------------------------------------------------------
 
-_per_directory_history_directory="$HISTORY_BASE${PWD:A}/history"
-
 function _per-directory-history-change-directory() {
-  _per_directory_history_directory="$HISTORY_BASE${PWD:A}/history"
-  mkdir -p ${_per_directory_history_directory:h}
   if [[ $_per_directory_history_is_global == false ]]; then
-    #save to the global history
+    # Save to the global history
     fc -AI $HISTFILE
-    #save history to previous file
-    local prev="$HISTORY_BASE${OLDPWD:A}/history"
-    mkdir -p ${prev:h}
-    fc -AI $prev
-
-    #discard previous directory's history
+    
+    # Determine previous history file based on OLD directory
+    local prev_history_path=$(_get_history_file_for_directory "$OLDPWD")
+    
+    # Make sure the directory exists
+    if [[ $prev_history_path == */* ]]; then
+      mkdir -p ${prev_history_path:h}
+    fi
+    
+    # Save history to previous directory's history file
+    fc -AI "$prev_history_path"
+    
+    # Set the new history file path based on current directory
+    _set_per_directory_history_path
+    
+    # Discard previous directory's history
     local original_histsize=$HISTSIZE
     HISTSIZE=0
     HISTSIZE=$original_histsize
 
-    #read history in new file
+    # Read history in new file
     if [[ -e $_per_directory_history_directory ]]; then
       fc -R $_per_directory_history_directory
     fi
+  else
+    # Even when using global history, update the directory path
+    _set_per_directory_history_path
   fi
 }
 
 function _per-directory-history-addhistory() {
-  # respect hist_ignore_space
+  # Respect hist_ignore_space
   if [[ -o hist_ignore_space ]] && [[ "$1" == \ * ]]; then
       true
   else
       print -Sr -- "${1%%$'\n'}"
-      # instantly write history if set options require it.
+      # Instantly write history if set options require it.
       if [[ -o share_history ]] || \
          [[ -o inc_append_history ]] || \
          [[ -o inc_append_history_time ]]; then
@@ -132,6 +177,9 @@ function _per-directory-history-addhistory() {
 function _per-directory-history-precmd() {
   if [[ $_per_directory_history_initialized == false ]]; then
     _per_directory_history_initialized=true
+    
+    # Initial setup of history file path
+    _set_per_directory_history_path
 
     if [[ $HISTORY_START_WITH_GLOBAL == true ]]; then
       _per-directory-history-set-global-history
@@ -163,13 +211,14 @@ function _per-directory-history-set-global-history() {
   fi
 }
 
-mkdir -p ${_per_directory_history_directory:h}
+# Initialize
+_set_per_directory_history_path
 
-#add functions to the exec list for chpwd and zshaddhistory
+# Add functions to the exec list for chpwd and zshaddhistory
 autoload -U add-zsh-hook
 add-zsh-hook chpwd _per-directory-history-change-directory
 add-zsh-hook zshaddhistory _per-directory-history-addhistory
 add-zsh-hook precmd _per-directory-history-precmd
 
-# set initialized flag to false
+# Set initialized flag to false
 _per_directory_history_initialized=false
